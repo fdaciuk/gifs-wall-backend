f8e53167af2c7a8e14c6d8b17d354bbca571d719
Fernando Daciuk <f.daciuk@gmail.com>
2022-10-10 20:11:31 -0300

    feat: save complex data on blockchain

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -11,8 +11,17 @@ pub mod myepicproject {
     Ok(())
   }
 
-  pub fn add_gif(ctx: Context<AddGif>) -> Result<()> {
+  pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> Result<()> {
     let base_account = &mut ctx.accounts.base_account;
+    let user = &mut ctx.accounts.user;
+
+    let item = ItemStruct {
+      gif_link,
+      // user_address: *user.key,
+      user_address: *user.to_account_info().key,
+    };
+
+    base_account.gif_list.push(item);
     base_account.total_gifs += 1;
     Ok(())
   }
@@ -31,9 +40,18 @@ pub struct StartStuffOff<'info> {
 pub struct AddGif<'info> {
   #[account(mut)]
   pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+}
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+  pub gif_link: String,
+  pub user_address: Pubkey,
 }
 
 #[account]
 pub struct BaseAccount {
   pub total_gifs: u64,
+  pub gif_list: Vec<ItemStruct>,
 }
--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -24,14 +24,16 @@ const main = async () => {
   let account = await program.account.baseAccount.fetch(baseAccount.publicKey)
   console.log('👀 GIF Count', account.totalGifs.toString())
 
-  await program.rpc.addGif({
+  await program.rpc.addGif('insert_a_gif_link_here', {
     accounts: {
       baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
     }
   })
 
   account = await program.account.baseAccount.fetch(baseAccount.publicKey)
   console.log('👀 GIF Count', account.totalGifs.toString())
+  console.log('👀 GIF List', account.gifList)
 }
 
 const runMain = async () => {
