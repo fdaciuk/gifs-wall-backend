85ca0af7109fcfcdf049ee1a38878f8c7d95d95d
Fernando Daciuk <f.daciuk@gmail.com>
2022-10-11 19:27:19 -0300

    feat: configure project to deploy on devnet

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("8oJLsdGcukkSvFor38wbJvkU3UUgGhsuadHSGJHW4PtZ");
 
 #[program]
 pub mod myepicproject {
